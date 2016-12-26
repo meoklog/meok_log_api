70722c9793541ba158898c59904987e13afae46e
meoklog <meoklog@gmx.com>
2016-12-26 10:59:28 +0100

    Adds smoke json response with CORS

--- src/meok_log_api.cr
+++ src/meok_log_api.cr
@@ -3,5 +3,5 @@ require "meok_log"
 # require "./meok_log_api/*"
 
 module MeokLogApi
-  # TODO Put your code here
+  
 end
--- src/meok_log_api/index.cr
+++ src/meok_log_api/index.cr
@@ -1,8 +1,15 @@
 
 require "kemal"
+require "json"
 
-get "/" do
-  "hello"
+get "/" do |env|
+  env.response.headers["Access-Control-Allow-Origin"] = "*"
+  env.response.content_type = "application/json"
+  [
+    { title: "github", url: "http://github.com" },
+    { title: "bitbucket", url: "http://bitbucket.org"},
+    { title: "gitlab", url: "http://gitlab.com" }
+  ].to_json
 end
 
 Kemal.run
